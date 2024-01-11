83b216aca81225da246f21b6d1bc9970c34aa87f
kokawaguchi <68247290+kokawaguchi@users.noreply.github.com>
2024-01-11 15:35:03 -0700

    Week 2 Completion
    week 2 completed

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Ko Kawaguchi</h1>
+        <div>
+            <Link href="https://github.com/kokawaguchi">Git Hub</Link>
+        </div>
+
+      </main>
+    );
+  }
