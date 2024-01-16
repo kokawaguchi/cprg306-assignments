1eeef5ff97fe6a0d2f841b830014b1cee6f93584
kokawaguchi <68247290+kokawaguchi@users.noreply.github.com>
2024-01-16 13:28:31 -0700

    Updated root page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,24 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
+  const weekAssignment = [2, 3, 4, 5, 6, 7, 8, 10];
+  const doneUpTo = 10;
+
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <div>
-        <Link href="/week-2">Week 2</Link>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
+        <h1 className="text-4xl font-bold mb-5">
+          CPRG 306: Web Development 2 - Assignments
+        </h1>
+        <div className="text-lg">
+          <ul>
+            {weekAssignment.map((week) => (
+              <li key={week} className="hover:text-green-400 hover:underline">
+                <Link href={`/week-${week}`}>Week {week} Assignment</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </main>
   );
