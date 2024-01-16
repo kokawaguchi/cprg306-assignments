58ee35b77ca9df7c10c216a07738cc710e5e1452
kokawaguchi <68247290+kokawaguchi@users.noreply.github.com>
2024-01-16 13:44:50 -0700

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <main>
-      <h1>Ko Kawaguchi</h1>
+      <h1 className="text-green-200 font-bold">Ko Kawaguchi</h1>
       <div>
         <Link href="https://github.com/kokawaguchi">https://github.com</Link>
       </div>
