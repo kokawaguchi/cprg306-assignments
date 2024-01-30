de5cac9a37143d2c324047d2cafa03b21557fd9b
kokawaguchi <68247290+kokawaguchi@users.noreply.github.com>
2024-01-30 16:41:15 -0700

    Week 4 complete

---
+++ app/week-4/page.js
@@ -0,0 +1,13 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <main className="m-4">
+      <div>
+        <h2>
+          <NewItem />
+        </h2>
+      </div>
+    </main>
+  );
+}
