7cd2c30c2dfb39b53ea6ea86a094a5afd88ff322
kokawaguchi <68247290+kokawaguchi@users.noreply.github.com>
2024-03-05 16:07:31 -0700

    Update item.js

--- app/week-7/item.js
+++ app/week-7/item.js
@@ -11,8 +11,7 @@ export default function Item({ name, quantity, category, onSelect }) {
   return (
     <div
       onClick={handleClick}
-      className={`bg-${divColour} hover:bg-blue-100 p-2 rounded-md mb-2 max-w-sm w-full bg-blue-900`}
-      style={{ transition: "background-color 0.3s" }}
+      className=" hover:bg-blue-100 p-2 rounded-md mb-2 max-w-sm w-full bg-blue-900"
     >
       <h3 className="text-xl font-bold">{name}</h3>
       <p className="text-sm">
