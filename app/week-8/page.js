a1d5192b4d27783bba933de3ba3ded2b319bfbfc
kokawaguchi <68247290+kokawaguchi@users.noreply.github.com>
2024-03-11 13:55:13 -0600

    Week 8 complete

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
 
 export default function Page() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
@@ -8,18 +9,34 @@ export default function Page() {
   console.log(user);
 
   return (
-    <div>
-      <h1>Week 8</h1>
-      <p>{user ? "Hi there!" : "Please sign in"}</p>
-      <p>{user?.email}</p>
-      {user && user.displayName}
-      <p>
+    <main className="h-screen flex items-center justify-center">
+      <div className="h-56 flex flex-col items-center">
+        <p>{user?.email}</p>
         {user ? (
-          <button onClick={firebaseSignOut}>Sign Out</button>
+          <div className="flex flex-col items-center">
+            <div>
+              <a className="hover:text-pink-100 text-cyan-900">
+                <Link href="/week-8/shopping-list">Shopping List</Link>
+              </a>
+            </div>
+            <button
+              onClick={firebaseSignOut}
+              className="text-cyan-900 hover:text-pink-100 "
+            >
+              Sign Out
+            </button>
+          </div>
         ) : (
-          <button onClick={gitHubSignIn}>Sign In with GitHub</button>
+          <div>
+            <button
+              onClick={gitHubSignIn}
+              className="text-cyan-900 hover:text-pink-100"
+            >
+              Sign In with GitHub
+            </button>
+          </div>
         )}
-      </p>
-    </div>
+      </div>
+    </main>
   );
 }
